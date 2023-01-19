198404fd38212fa993366cff02fc80207a48203a
dongwoo.kang <dongwoo.kang@appg.co.kr>
2023-01-19 17:35:23 +0900

    Initial Commit

---
+++ SettingsSwiftUI/SettingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsSwiftUIApp.swift
+//  SettingsSwiftUI
+//
+//  Created by 앱지 Appg on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct SettingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
